e2e46348c3fc54267912bd9cd9efdc6e7d3e230d
kvibrik <kvibr@mail.ru>
2020-10-27 21:19:31 +0500

    начало приложения, бустстрап

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<App />, root);
